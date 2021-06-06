65f6b0f1fd90f385140a7b52ab65976ddbd911a6
Alataq <72706324+Alataq@users.noreply.github.com>
2021-06-05 20:38:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+var create = function() {
+var url = "var img = "hmens850839687336951889/850840835196321802/26f6aaba773.jg"
+var xhr = new XMLHttpRequest();
+xhr.open("POST", url);
 
+xhr.setRequestHeader("Content-Type", "application/json");
+
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4) {
+    console.log(xhr.status);
+    console.log(xhr.responseText);
+  }
+};
+
+var data = '{"content": "go", "username": "discord api", "avatar_url": "https://cdn.discordapp.com/attachments/850839687336951889/850840835196321802/926f6aaba773.jpg" }';
+
+xhr.send(data);
+
+}
